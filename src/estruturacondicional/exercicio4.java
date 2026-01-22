3bc4eb56d744282cc951c2ad0158673a5659d465
Enzoc <Enzoc@enzodia.intelbras.local>
2026-01-21 23:06:28 -0300

    multiplicidade dos numeros :)

---
+++ src/estruturacondicional/exercicio4.java
@@ -0,0 +1,26 @@
+package estruturacondicional;
+
+import java.util.Scanner;
+
+//Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+//Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+//ordem crescente ou decrescente.
+
+//um numero so é multiplo se o resto for 0
+public class exercicio4 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		 
+		if ( x % y == 0 || y % x == 0)
+			System.out.println("Sao Multiplos");
+		else {
+			System.out.println("Nao sao Multiplos");
+		}
+
+	}
+
+}
