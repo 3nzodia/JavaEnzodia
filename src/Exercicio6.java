632b045ac8dab7bd8dfab0d741e6f22c29a32e1f
Enzoc <Enzoc@enzodia.intelbras.local>
2026-01-22 23:47:04 -0300

    exercicio utilizando scanner e logica sobre uso do if

---
+++ src/Exercicio6.java
@@ -0,0 +1,42 @@
+/*Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+seguir, calcule e mostre o valor da conta a pagar.
+*/
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int codigo, quantidade;
+		double valor=0;
+
+		System.out.println("Digite o codigo do pedido , e logo em seguida a quantidade");
+		codigo = sc.nextInt();
+		quantidade = sc.nextInt();
+
+		if (codigo == 1) {
+			 valor += 4 * quantidade;
+			System.out.printf("Total: R$ %.2f", valor);
+		}
+		if (codigo == 2) {
+			valor += 4.50 * quantidade;
+			System.out.printf("Total: R$ %.2f",valor);
+		}
+		if (codigo == 3) {
+			valor += 5 * quantidade;
+			System.out.printf("Total: R$ %.2f",valor);
+		}
+		if (codigo == 4) {
+			valor += 2 * quantidade;
+			System.out.printf("Total: R$ %.2f",valor);
+		}
+		if (codigo == 5) {
+			valor += 1.50 * quantidade;
+			System.out.printf("Total: R$ %.2f",valor);
+		}
+
+	}
+
+}
