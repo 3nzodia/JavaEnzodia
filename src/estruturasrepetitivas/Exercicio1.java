519e92ec437862c7d32f651945cd85b2535fe251
Enzoc <Enzoc@enzodia.intelbras.local>
2026-01-27 00:14:25 -0300

    Exercicio Referente a playlist de estruturasrepetitivas :3

---
+++ src/estruturasrepetitivas/Exercicio1.java
@@ -0,0 +1,28 @@
+package estruturasrepetitivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/* Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+*/
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x=sc.nextInt();	
+			while(x != 2002) {
+				System.out.println("Senha Invalida");
+				x = sc.nextInt();
+			
+				
+				
+			}
+		
+			System.out.println("Acesso Permitido");
+			sc.close();
+	}
+}
