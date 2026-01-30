63ae5fa52a001282d5563103560ee201257dc2bd
Enzo camacho <enzocamacho88@gmail.com>
2026-01-30 02:35:09 -0300

    extra

---
+++ src/estruturasrepetitivas/ExercicioExtra.java
@@ -0,0 +1,19 @@
+//Ler um número inteiro N e calcular todos os seus divisores
+
+package estruturasrepetitivas;
+
+import java.util.Scanner;
+
+public class ExercicioExtra {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			if (N % i == 0) 
+				System.out.println(i);
+			
+		}
+		sc.close();
+	}
+}
